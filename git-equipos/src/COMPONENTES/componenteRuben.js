c8f65a5217c1ddbca5d513ddda05cf6111027e37
rubenFdzC <rubenfdz1998@gmail.com>
2023-05-04 23:50:08 +0200

    añado h2

--- git-equipos/src/COMPONENTES/componenteRuben.js
+++ git-equipos/src/COMPONENTES/componenteRuben.js
@@ -4,6 +4,7 @@ const componenteRuben = () => {
   return (
     <div>
       <h1>COMPONENTE RUBEN FUNCIONANDO</h1>
+      <h2>ESTO NO LE APARECIA A VICTOR Y AHORA SI DEBERÍA </h2>
     </div>
   )
 }
