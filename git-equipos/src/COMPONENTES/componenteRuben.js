2c33954153867b631939c7bc1e17be7faa3c4947
rubenFdzC <rubenfdz1998@gmail.com>
2023-05-05 12:19:37 +0200

    Cambio en comp

--- git-equipos/src/COMPONENTES/componenteRuben.js
+++ git-equipos/src/COMPONENTES/componenteRuben.js
@@ -5,6 +5,7 @@ const componenteRuben = () => {
     <div>
       <h1>COMPONENTE RUBEN FUNCIONANDO</h1>
       <h2>ESTO NO LE APARECIA A VICTOR Y AHORA SI DEBERÍA </h2>
+      <p>Cambio no importante</p>
     </div>
   )
 }
